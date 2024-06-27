95f277ec0ecc262e219fde0250460ddb2e657497
Angelo <angelo@wemaron.nl>
2024-06-27 20:28:51 +0200

    list is ready

--- migrations/001_initial_setup.sql
+++ migrations/001_initial_setup.sql
@@ -1,7 +1,12 @@
--- migrations/001_initial_setup.sql
+CREATE TABLE login_credentials (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE userss (
-    id INT PRIMARY KEY IDENTITY,
-    username NVARCHAR(255) NOT NULL,
-    password_hash NVARCHAR(255) NOT NULL
+CREATE TABLE user_information (
+    info_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES login_credentials(user_id)
 );
